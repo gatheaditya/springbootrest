d72f7a6cbb55d8e8ec1c555b8878d161ad743353
gatheaditya <gatheaditya@gmail.com>
2017-11-19 19:29:47 +0530

    added model

--- src/main/java/model/companyModel.java
+++ src/main/java/model/companyModel.java
@@ -1,9 +1,33 @@
 package model;
 
+import java.util.Collection;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class companyModel {
+	
+	private String username;
+	private  String password;
+	private Collection <String> roles;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Collection<String> getRoles() {
+		return roles;
+	}
+	public void setRoles(Collection<String> roles) {
+		this.roles = roles;
+	}
 	
 	
 	
