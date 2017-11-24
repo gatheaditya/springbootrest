170333f5517a15256a649205cc1e092218b23444
gatheaditya <gatheaditya@gmail.com>
2017-11-24 18:39:37 +0530

    exposed all end point without security
    done

--- src/main/java/controller/WebSecurityConfig.java
+++ src/main/java/controller/WebSecurityConfig.java
@@ -13,7 +13,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests().anyRequest().authenticated()
+            .authorizeRequests().antMatchers("/all")
+            .permitAll()
+            .anyRequest().authenticated()
                 .and()
             .formLogin()      
                 .permitAll()
